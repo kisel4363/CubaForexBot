02c0fb73b7feab74db14912c78c41542c75f5ffb
kisel4363 <kisel4363@gmail.com>
2024-10-01 09:49:54 -0400

    + Bot posting buys and listing orders

--- src/CubaForexBot.ts
+++ src/CubaForexBot.ts
@@ -3,15 +3,20 @@ import { Bot, CommandContext, Context, InlineKeyboard, webhookCallback } from "g
 
 const TELEGRAM_TOKEN="7489205472:AAErIX--3QpSMZaYqhIRM0cOo76L9FRnsRE";
 
+interface Operator { id: number, first_name: string, last_name: string | undefined, username: string | undefined }
+interface Order { operator?: Operator, operation?:string, p_currency?: string, amount?: number, s_currency?: string, price?: number, date?: Date };
 export class CubaForexBot{
     private bot: Bot;
     private amounts = [ "5", "10", "20", "100"];
     private currencies = ["USD", "EUR", "CAD", "BTC", "USDT", "ETH"];
-    private storedMessage = ""
+    private postedOperations: Order[] = []
+
     constructor(){
         this.bot = new Bot(TELEGRAM_TOKEN);
 
         this.setUpBuyCommand( this.bot );
+
+        this.setUpShowOrdersCommand( this.bot )
 
         this.listenSelectPrincipalCurrency( this.bot );
 
@@ -22,6 +27,10 @@ export class CubaForexBot{
         this.listenSelectSecundaryCurrency( this.bot );
 
         this.listenSelectPrice( this.bot );
+
+        this.listenFinishSelectPrice( this.bot );
+
+        this.listenConfirmOperation( this.bot );
 
         this.setUpAllCommands(this.bot);
 
@@ -48,11 +57,9 @@ export class CubaForexBot{
 
     private replyAllOtherMessagesAndStartCmd(bot: Bot){
         const introductionMessage = `
-        Hello! I'm a Telegram bot.
-        I'm powered by Cyclic, the next-generation serverless computing platform.
-            <b>Commands</b>
-            /buy
-            /comprar`;
+        Compra divisas: /buy
+        Compra divisas: /comprar
+        `;
         
         const aboutUrlKeyboard = new InlineKeyboard().url(
             "Host your own bot for free.",
@@ -82,34 +89,26 @@ export class CubaForexBot{
       
         const keyboard = options.reply_markup;
 
-        const data = 'operation:buy';
+        const data = 'op:buy';
 
-        this.selectCurrencyKeyboard( keyboard, data, this.currencies, 'p_currency' );
+        this.selectCurrencyKeyboard( keyboard, data, this.currencies, 'pc' );
         
         ctx.reply( message, options );
     }
 
-    private selectCurrencyKeyboard = (keyboard: InlineKeyboard, incData: string, currencies: string[], type: 'p_currency' | 's_currency') => {
-        currencies.forEach( (currency, index) => {
-            const data = `${incData} ${type}:${currency}`
-            
-            keyboard.text( currency, data );
-            
-            if( index === 2 )
-                keyboard.row();
-        })
-    }
-
     private listenSelectPrincipalCurrency(bot: Bot){
-        const trigger = /operation:.* p_currency:(\w+)$/;
+        // op => operation 
+        // pc => principal cur
+
+        const trigger = /op:\w+ pc:\w+$/;
         bot.callbackQuery( trigger, ctx => {
             const data = ctx.callbackQuery.data;
             //@ts-ignore
-            let [input, operation, currency] = data.match(/operation:(.*) p_currency:(.*)/) as string[];
+            let [input, op, currency] = data.match(/op:(.*) pc:(.*)/) as string[];
             console.log("listenSelectPrincipalCurrency")
-            this.storedMessage = "Message in first listener";
-            operation = operation === "buy"? "comprar" : "vender";
-            const message = `Que cantidad de ${currency} desea ${operation}?`;
+
+            op = op === "buy"? "comprar" : "vender";
+            const message = `Que cantidad de ${currency} desea ${op}?`;
             const options = { reply_markup: new InlineKeyboard() };
         
             const keyboard = options.reply_markup;
@@ -119,9 +118,55 @@ export class CubaForexBot{
         } )
     }
 
+    private listenSetPrincipalCurrencyAmount( bot: Bot ) {
+        const trigger = /op:(\w+) pc:(\w+) d:(\d+(\.)?(\d+)?)$/;
+        const queryMatch = /op:(.*) pc:(.*) d:(.*)/;
+        bot.callbackQuery( trigger, ctx => {
+            const data = ctx.callbackQuery.data;
+            let [input, operation, p_currency, digiting] = data.match(queryMatch) as string[];
+            
+            console.log('listenSetPrincipalCurrencyAmount')
+
+            operation = operation === "buy"? "comprar" : "vender";
+
+            const message = `Quiero ${operation} ${digiting} ${p_currency}!`;
+            const options = { reply_markup: new InlineKeyboard() };
+        
+            const keyboard = options.reply_markup;
+            this.selectAmountKeyboard( keyboard, data )
+            
+            ctx.editMessageText( message, options )
+        })
+    }
+
+    private listenFinishSetPrincipalCurrencyAmount( bot: Bot ) {
+        const trigger = /op:(\w+) pc:(\w+) d:(\d+(\.)?(\d+)?) n/;
+        const queryMatch = /op:(.*) pc:(.*) d:(.*) n/;
+        bot.callbackQuery( trigger, ctx => {
+            let data = ctx.callbackQuery.data;
+            let [input, operation, p_currency, digiting] = data.match(queryMatch) as string[];
+
+            data = `op:${operation} pc:${p_currency} am:${digiting}`
+            console.log("listenFinishSetPrincipalCurrencyAmount")
+            let message;
+            if (operation === "buy" )
+                message = "Con que moneda va a pagar?";
+            else 
+                message = "Que moneda desea recibir?";
+
+            const options = { reply_markup: new InlineKeyboard() };
+        
+            const keyboard = options.reply_markup;
+
+            this.selectCurrencyKeyboard( keyboard, data, this.currencies, 'sc')
+            
+            ctx.editMessageText( message, options )
+        })
+    }
+
     private listenSelectSecundaryCurrency(bot: Bot){
-        const trigger = /operation:.* p_currency:.* amount:.* s_currency:(\w+)$/;
-        const queryMatch = /operation:(.*) p_currency:(.*) amount:(.*) s_currency:(.*)/;
+        const trigger = /op:\w+ pc:\w+ am:\d+(\.)?(\d+)? sc:(\w+)$/;
+        const queryMatch = /op:(.*) pc:(.*) am:(.*) sc:(.*)/;
         bot.callbackQuery( trigger, ctx => {
             const data = ctx.callbackQuery.data;
             //@ts-ignore
@@ -143,55 +188,124 @@ export class CubaForexBot{
     }
 
     private listenSelectPrice( bot: Bot ){
-        // (\d+(\.)?(\d+)?)$
-        const trigger = /operation:.* p_currency:.* amount:.* s_currency:.* digiting:.*/;
-        const queryMatch = /operation:(.*) p_currency:(.*) amount:(.*) s_currency:(.*) digiting:(.*)/;
+        const trigger = /op:\w+ pc:\w+ am:\d+(\.)?(\d+)? sc:(\w+) d:(\d+(\.)?(\d+)?)$/;
+        const queryMatch = /op:(.*) pc:(.*) am:(.*) sc:(.*) d:(.*)/;
 
         bot.callbackQuery( trigger, ctx => {
             console.log("listenSelectPrice")
             const data = ctx.callbackQuery.data;
             let [input, operation, p_currency, amount, s_currency, digiting] = data.match(queryMatch) as string[];
 
-            console.log({operation, p_currency, amount, s_currency, digiting})
+            operation = operation === "buy"? "Comprar" : "Vender";
 
-            operation = operation === "buy"? "comprar" : "vender";
+            const message = `${operation} ${p_currency} a ${digiting} ${s_currency}`;
 
-            const message = `Quiero ${operation} ${amount} ${p_currency} a un precio de ${digiting} ${s_currency}!`;
-
-            console.log("MESSAGE", message)
             const options = { reply_markup: new InlineKeyboard() };
         
             const keyboard = options.reply_markup;
-            // this.selectAmountKeyboard( keyboard, data)
-            ctx.reply(`Price ${digiting} selected` )
-            
-            // ctx.editMessageText( message, options )
+            this.selectAmountKeyboard( keyboard, data );
+
+            ctx.editMessageText( message, options )
         })
     }
 
-    // private parseQueryDataToObjec(data: string){
-    //     // example: data = 'operation:buy p_currency:USD amount:50.984 s_currency:CUP price:93.423'
-    //     const obj = data.split(' ')
-    //         .reduce( ( obj, pair ) => {
-    //             const [key, value] = pair.split(':'); 
-    //             obj[key] = value;  
-    //             return obj; 
-    //         }, {} );
+    private listenFinishSelectPrice( bot: Bot ){
         
-    //     return obj
-    // }
+        const trigger = /op:\w+ pc:\w+ am:\d+(\.)?(\d+)? sc:(\w+) d:(\d+(\.)?(\d+)?) n/;
+        const queryMatch = /op:(.*) pc:(.*) am:(.*) sc:(.*) d:(.*) n/;
 
-    private selectAmountKeyboard( keyboard: InlineKeyboard, incData: string){
+        bot.callbackQuery( trigger, ctx => {
+            console.log("listenFinishSelectPrice")
+            let data = ctx.callbackQuery.data;
+            let [input, operation, p_currency, amount, s_currency, digiting] = data.match(queryMatch) as string[];
+
+            data = `op:${operation} pc:${p_currency} am:${amount} sc:${s_currency} pr:${digiting}`
+            operation = operation === "buy"? "Comprar" : "Vender";
+
+            const message = `${operation} ${amount} ${p_currency} a ${digiting} ${s_currency}`;
+
+            const options = { reply_markup: new InlineKeyboard() };
+        
+            const keyboard = options.reply_markup;
+            this.confirmKeyboard( keyboard, data );
+
+            ctx.editMessageText( message, options )
+        })
+    }
+
+    private listenConfirmOperation( bot: Bot ){
+        const trigger = /op:\w+ pc:\w+ am:\d+(\.)?(\d+)? sc:(\w+) pr:\d+(\.)?(\d+)? conf:\w+/;
+        const queryMatch = /op:(.*) pc:(.*) am:(.*) sc:(.*) pr:(.*) conf:(.*)/;
+
+        bot.callbackQuery( trigger, ctx => {
+            console.log("listenConfirmOperation")
+            let data = ctx.callbackQuery.data;
+            let [input, operation, p_currency, amount, s_currency, price, confirm] = data.match(queryMatch) as string[];
+
+            data = `op:${operation} pc:${p_currency} am:${amount} sc:${s_currency} pr:${price}`
+            const opMessage = operation === "buy"? "Compra" : "Venta";
+
+
+            let message: string;
+
+            if (confirm === "yes"){
+                message = `Has publicado una ${opMessage} de ${amount} ${p_currency} a ${price} ${s_currency}`;
+                const operator: Operator = {
+                    id: ctx.from.id,
+                    first_name: ctx.from.first_name,
+                    last_name: ctx.from.last_name,
+                    username: ctx.from.username
+                }
+                const order: Order = {operator, operation, p_currency, amount: Number(amount), s_currency, price: Number(price), date: new Date()}
+                this.postedOperations.push(order)
+            } else {
+                message = "Operacion cancelada"
+            }
+
+            ctx.editMessageText(message)
+        })
+    }
+    // end:: Buy command
+
+    // start:: Show ops commad
+    private setUpShowOrdersCommand(bot: Bot){
+        bot.command("orders", this.handleShowOrdersCommand );
+    }
+
+    private handleShowOrdersCommand = (ctx: CommandContext<Context>) => {
+        let message = `======== ORDENES ========\n`;
+
+        this.postedOperations.forEach( ( order, index ) => {
+            message = `${message} ${index+1}: ${order.operation} ${order.amount} ${order.p_currency} a ${order.price} ${order.s_currency}\n`;
+        })
+        
+        ctx.reply( message );
+    }
+    // end:: Show ops commad
+
+    // start:: Utils
+    private selectCurrencyKeyboard = (keyboard: InlineKeyboard, incData: string, currencies: string[], type: 'pc' | 'sc') => {
+        currencies.forEach( (currency, index) => {
+            const data = `${incData} ${type}:${currency}`
+            
+            keyboard.text( currency, data );
+            
+            if( index === 2 )
+                keyboard.row();
+        })
+    }
+
+    private selectAmountKeyboard( keyboard: InlineKeyboard, incData: string ){
         let data;
         
         ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0', '<']
         .forEach( char => {
             const pairs = incData.split(' ');
-            const digitPairIndex = pairs.findIndex( p => p.split(':')[0] === 'digiting' )
+            const digitPairIndex = pairs.findIndex( p => p.split(':')[0] === 'd' )
 
-            if( digitPairIndex === -1 )
-                data = `${incData} digiting:${char}`
-            else {
+            if( digitPairIndex === -1 ){
+                data = `${incData} d:${char}`
+            } else {
                 let digitingPair = pairs.splice(digitPairIndex, 1)[0];
                 let [key, value] = digitingPair.split(':');
 
@@ -205,68 +319,29 @@ export class CubaForexBot{
                 data = pairs.join(' ');
             }
 
+            
             keyboard.text( char, data );
+            
             if( ['3', '6', '9'].includes(char) )
                 keyboard.row();
         })
 
-        keyboard.row()
-        keyboard.text('next', `${incData} next`)
+        keyboard.row();
+        keyboard.text('next', `${incData} n`)
     }
 
-    private listenSetPrincipalCurrencyAmount( bot: Bot ) {
-        const trigger = /operation:.* p_currency:(\w+) digiting:(\d+(\.)?(\d+)?)$/;
-        const queryMatch = /operation:(.*) p_currency:(.*) digiting:(.*)/;
-        bot.callbackQuery( trigger, ctx => {
-            const data = ctx.callbackQuery.data;
-            let [input, operation, p_currency, digiting] = data.match(queryMatch) as string[];
-            
-            console.log('listenSetPrincipalCurrencyAmount')
-            console.log("SOTRED_MESSAGE", this.storedMessage)
-
-            operation = operation === "buy"? "comprar" : "vender";
-
-            const message = `Quiero ${operation} ${digiting} ${p_currency}!`;
-            const options = { reply_markup: new InlineKeyboard() };
-        
-            const keyboard = options.reply_markup;
-            this.selectAmountKeyboard( keyboard, data)
-            
-            ctx.editMessageText( message, options )
-        })
+    private confirmKeyboard( keyboard: InlineKeyboard, incData: string ){
+        keyboard.text("Confirmar", `${incData} conf:yes`)
+        keyboard.text("Descartar", `${incData} conf:no`)
     }
-
-    private listenFinishSetPrincipalCurrencyAmount( bot: Bot ) {
-        const trigger = /operation:.* p_currency:.* digiting:.* next/;
-        const queryMatch = /operation:(.*) p_currency:(.*) digiting:(.*) next/;
-        bot.callbackQuery( trigger, ctx => {
-            let data = ctx.callbackQuery.data;
-            let [input, operation, p_currency, digiting] = data.match(queryMatch) as string[];
-
-            data = `operation:${operation} p_currency:${p_currency} amount:${digiting}`
-            console.log("listenFinishSetPrincipalCurrencyAmount")
-            let message;
-            if (operation === "buy" )
-                message = "Con que moneda va a pagar?";
-            else 
-                message = "Que moneda desea recibir?";
-
-            const options = { reply_markup: new InlineKeyboard() };
-        
-            const keyboard = options.reply_markup;
-
-            this.selectCurrencyKeyboard( keyboard, data, this.currencies, 's_currency')
-            
-            ctx.editMessageText( message, options )
-        })
-    }
-    // end:: Buy command
+    // end:: Utils
 
     // start:: Set up bot commands
     private setUpAllCommands(bot: Bot){
         bot.api.setMyCommands([
-            { command: "buy", description: "Comprar divisas"},
-            { command: "comprar", description: "Comprar divisas"}
+            // { command: "buy", description: "Comprar divisas"},
+            { command: "comprar", description: "Comprar divisas"},
+            { command: "orders", description: "Ver ordenes"}
         ]);
     }
     // end:: Set up bot commands
